f8f6423fa6a4934986c07debd8fbdb683b62640c
renghen <renghen@yahoo.com>
2025-01-12 13:18:42 +0400

    session ops added, test to be done

--- src/main/scala/com/renghen/session/Session.scala
+++ src/main/scala/com/renghen/session/Session.scala
@@ -7,16 +7,14 @@ import java.time.LocalDateTime
 
 final case class Session(
     id: UUID,
-    valid: Boolean,
     customer: CustomerDataResponse,
     lastAccess: LocalDateTime)
 
 enum SessionOpErrors:
-  case SessionNotFound, SessionExpired
+  case SessionNotFound, SessionExpired, SessionAlreadyExist
 end SessionOpErrors
 
 trait SessionOps:
-  def create(customer: CustomerDataResponse): Session
-  def disable(id: UUID): Either[SessionOpErrors, Session]
-  def isvalid(id: UUID): Either[SessionOpErrors, Session]
+  def create(customer: CustomerDataResponse): Either[SessionOpErrors, Session]
+  def remove(id: UUID): Either[SessionOpErrors, Session]
 end SessionOps
--- src/main/scala/com/renghen/session/SessionMemoryLive.scala
+++ src/main/scala/com/renghen/session/SessionMemoryLive.scala
@@ -1,5 +1,28 @@
 package com.renghen.session
 
 import collection.mutable.HashMap
+import java.util.UUID
+import java.time.LocalDateTime
 
-class SessionMemoryLive {}
+import com.renghen.customer.CustomerDataResponse
+
+class SessionMemoryLive extends SessionOps:
+
+  override def create(customer: CustomerDataResponse): Either[SessionOpErrors, Session] =
+    registry.values.find(sess => sess.customer.username == customer.username) match
+      case None        =>
+        val uuid    = UUID.randomUUID()
+        val session = Session(uuid, customer, LocalDateTime.now())
+        registry.addOne(uuid, session)
+        Right(session)
+      case Some(value) => Left(SessionOpErrors.SessionAlreadyExist)
+
+  override def remove(id: UUID): Either[SessionOpErrors, Session] =
+    registry.find((key, value) => key == id) match
+      case None             => Left(SessionOpErrors.SessionNotFound)
+      case Some(_, session) =>
+        registry.remove(id)
+        Right(session)
+
+  private val registry = HashMap.empty[UUID, Session]
+end SessionMemoryLive
